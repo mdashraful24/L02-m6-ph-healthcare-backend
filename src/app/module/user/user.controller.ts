6bf029b667d51d01a3800d853f9f5ff376efa0b5
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-08-15 02:47:33 +0600

    added how to file upload will happen, introducing to multer and cloudinary with configuring both, creating and deleting user progile picture in cloudinary

---
+++ src/app/module/user/user.controller.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+import httpStatus from 'http-status';
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
+import { UserServices } from "./user.service";
+
+const uploadProfilePicture = catchAsync(async (req: Request, res: Response) => {
+    if (!req.file) {
+        throw new Error("No file uploaded");
+    }
+
+    const userId = req.user?.userId;
+
+    const result = await UserServices.uploadProfilePicture(req.file?.buffer as Buffer, userId!);
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Profile picture uploaded successfully",
+        data: result,
+    });
+});
+
+
+export const UserController = {
+    uploadProfilePicture,
+};
---
+++ src/app/module/user/user.route.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { upload } from "../../lib/multer";
+import { UserController } from "./user.controller";
+import { auth } from "../../middleware/checkAuth";
+import { Role } from "../../../generated/prisma/enums";
+
+const router = Router();
+
+router.patch("/upload-profile-picture",
+    auth(Role.PATIENT, Role.DOCTOR, Role.ADMIN, Role.SUPER_ADMIN),
+    upload.single("profilePicture"),
+    UserController.uploadProfilePicture
+);
+
+export const UserRoutes = router;
